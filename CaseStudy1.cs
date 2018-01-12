2d75d38f33f9c8f2d9183226a171b85462174502
DakotaDecker <33061529+DakotaDecker@users.noreply.github.com>
2018-01-11 21:50:10 -0600

    Upload the first half of Chapter 1's Case Study
    This program just writes the town motto to the console.

---
+++ CaseStudy1.cs
@@ -0,0 +1,15 @@
+/*
+	Program written by: Dakota Decker
+	Program date: 1/11/18
+	Program description: Writes Greenville's motto to the console
+	Encountered issues: -
+*/
+
+using static System.Console;
+class Motto
+{
+	static void Main()
+	{
+		Write("The stars shine in Greenville");
+	}
+}
