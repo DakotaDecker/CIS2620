5955172a55cdd359051c9ff0c3131596917ae542
cdp387 <33994778+cdp387@users.noreply.github.com>
2018-01-12 21:18:11 -0600

    Update CaseStudy1.cs
    Updated to include asterisk border surrounding motto.

--- CaseStudy1.cs
+++ CaseStudy1.cs
@@ -4,12 +4,20 @@
 	Program description: Writes Greenville's motto to the console
 	Encountered issues: -
 */
+/* 
+ * Program written by: Eric Bjerke
+ * Program date: 1-12-18
+ * Program description: Ch1 Case 1, Program 2 "GreenvilleMotto2"
+ * Encounterd issues: None
+ */
 
-using static System.Console;
+sing static System.Console;
 class Motto
 {
 	static void Main()
 	{
-		Write("The stars shine in Greenville");
+	    WriteLine("*********************************");
+            WriteLine("* The stars shine in Greenville *");
+            WriteLine("*********************************");
 	}
 }
