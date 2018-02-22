8780326f1ddbc20a7d6cd6704fed42b618bf833b
cdp387 <33994778+cdp387@users.noreply.github.com>
2018-02-21 19:29:32 -0600

    Changed second prompt to ask for "last" year

--- GreenvilleRevenueWeek6.cs
+++ GreenvilleRevenueWeek6.cs
@@ -1,38 +1,54 @@
-//initialize variables
-double FEE = 25, revenue;
-int thisYearContestantsCount, lastYearContestantsCount;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
 
-//get numbers, check if between 0-30
-do
+namespace Case_Study_1
 {
-    Write("Enter number of contestants this year (0-30): ");
-    thisYearContestantsCount = Convert.ToInt32(ReadLine());
-}
-while (thisYearContestantsCount < 0 || thisYearContestantsCount > 30);
-do
-{
-    Write("Enter number of contestants this year (0-30): ");
-    lastYearContestantsCount = Convert.ToInt32(ReadLine());
-}
-while (lastYearContestantsCount < 0 || lastYearContestantsCount > 30);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //initialize variables
+            double FEE = 25, revenue;
+            int thisYearContestantsCount, lastYearContestantsCount;
 
-//calculate revenue
-revenue = FEE * thisYearContestantsCount;
+            //get numbers, check if between 0-30
+            do
+            {
+                Write("Enter number of contestants this year (0-30): ");
+                thisYearContestantsCount = Convert.ToInt32(ReadLine());
+            }
+            while (thisYearContestantsCount < 0 || thisYearContestantsCount > 30);
+            do
+            {
+                Write("Enter number of contestants last year (0-30): ");
+                lastYearContestantsCount = Convert.ToInt32(ReadLine());
+            }
+            while (lastYearContestantsCount < 0 || lastYearContestantsCount > 30);
 
-WriteLine("Estimated " + revenue.ToString("C2") +
-    " in entrance fee revenue this year.");
+            //calculate revenue
+            revenue = FEE * thisYearContestantsCount;
 
-if (thisYearContestantsCount > (lastYearContestantsCount * 2))
-{
-    WriteLine("The competition is more than twice as big this year!");
-}
-else if ((thisYearContestantsCount > lastYearContestantsCount) && (thisYearContestantsCount < (lastYearContestantsCount * 2)))
-{
-    WriteLine("The competition is bigger than ever!");
-}
-else
-{
-    WriteLine("A tighter race this year! Come out and cast your vote!");
-}
+            WriteLine("Estimated " + revenue.ToString("C2") +
+                " in entrance fee revenue this year.");
 
-ReadLine();
+            if (thisYearContestantsCount > (lastYearContestantsCount * 2))
+            {
+                WriteLine("The competition is more than twice as big this year!");
+            }
+            else if ((thisYearContestantsCount > lastYearContestantsCount) && (thisYearContestantsCount < (lastYearContestantsCount * 2)))
+            {
+                WriteLine("The competition is bigger than ever!");
+            }
+            else
+            {
+                WriteLine("A tighter race this year! Come out and cast your vote!");
+            }
+
+            ReadLine();
+        }
+    }
+}
