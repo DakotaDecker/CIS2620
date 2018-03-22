e2bd38475b0c4c74b55c13ff705a618af5ebc28f
DakotaDecker <33061529+DakotaDecker@users.noreply.github.com>
2018-03-21 21:08:24 -0500

    Updated for Week 11
    I did the first two bullet points for this week, two more to go.

--- GreenvilleRevenueWeek6.cs
+++ GreenvilleRevenueWeek6.cs
@@ -1,7 +1,7 @@
 /*
     Eric Bjerke and Dakota Decker
-    2/22/18
-    Unit 5 Greenville Motto
+    3/21/18
+    Unit 7 Greenville Motto
 */
 
 using static System.Console;
@@ -15,6 +15,32 @@ namespace Case_Study_1
 {
     class Program
     {
+        public static int GetContestantNumber(string year)
+        {
+            int contestantCount;
+            do
+            {
+                Write("Enter number of contestants {0} year (0-30): ", year);
+                contestantCount = Convert.ToInt32(ReadLine());
+            }
+            while (contestantCount < 0 || contestantCount > 30);
+            return contestantCount;
+        }
+        public static void FindBiggerYear(int thisYear, int lastYear)
+        {
+            if (thisYear > (lastYear * 2))
+            {
+                WriteLine("The competition is more than twice as big this year!");
+            }
+            else if ((thisYear > lastYear) && (thisYear < (lastYear * 2)))
+            {
+                WriteLine("The competition is bigger than ever!");
+            }
+            else
+            {
+                WriteLine("A tighter race this year! Come out and cast your vote!");
+            }
+        }
         static void Main(string[] args)
         {
             //initialize variables
@@ -22,36 +48,40 @@ namespace Case_Study_1
             int thisYearContestantsCount, lastYearContestantsCount;
             int singers = 0, dancers = 0, musicians = 0, others = 0;
 
-            //get numbers for THIS year, check if between 0-30
-            do
-            {
-                Write("Enter number of contestants this year (0-30): ");
-                thisYearContestantsCount = Convert.ToInt32(ReadLine());
-            }
-            while (thisYearContestantsCount < 0 || thisYearContestantsCount > 30);
 
-            //initialize arrays
-            string[] contestants = new string[thisYearContestantsCount];
+            //get numbers, check if between 0-30
+            thisYearContestantsCount = GetContestantNumber("this");
+            lastYearContestantsCount = GetContestantNumber("last");
+
+            //calculate revenue
+            revenue = FEE * thisYearContestantsCount;
+
+            WriteLine("Estimated " + revenue.ToString("C2") +
+                " in entrance fee revenue this year.");
+
+            FindBiggerYear(thisYearContestantsCount, lastYearContestantsCount);
+
+            //initializes parallel arrays for contestants
+            string[] names = new string[thisYearContestantsCount];
             string[] talents = new string[thisYearContestantsCount];
 
-            //get name and talent for each contestant this year, add to respective arrays
-            for (int x = 0; x < thisYearContestantsCount; x++)
+            //asks for contestant names and talent code
+            for (int i = 0; i < thisYearContestantsCount; i++)
             {
-                Write("What is contestant #{0}'s name? ", x + 1);
-                string contestantName = ReadLine();
-                WriteLine("What is {0}'s talent code?", contestantName);
-                Write("Singing: S, Dancing: D, Musical Instrument: M, Other: O - ");
-                string talentCode = ReadLine();
-                while (talentCode != "S" && talentCode != "D" && talentCode != "M" && talentCode != "O")
+                Write("Enter contestant #{0} name: ", (i + 1));
+                names[i] = ReadLine();
+                string userInput;
+                WriteLine("What is {0}'s talent?", names[i]);
+                do
                 {
-                    Write("Invalid entry. Please try again. Singing: S, Dancing: D, Musical Instrument: M, Other: O - ");
-                    talentCode = ReadLine();
-                }                
-                contestants[x] = contestantName;
-                talents[x] = talentCode;
+                    Write("Enter 'S' for Singing, 'D' for dancing, 'M' for music, or 'O' for other: ");
+                    userInput = (ReadLine()).ToUpper();
+                }
+                while ((userInput != "S") && (userInput != "D") && (userInput != "M") && (userInput != "O"));
+                talents[i] = userInput;
 
                 //increment respective talent counter
-                switch (talentCode)
+                switch (userInput)
                 {
                     case "S":
                         singers += 1;
@@ -78,43 +108,22 @@ namespace Case_Study_1
             WriteLine("Number of others: {0}", others);
 
             //display names of contestants if user would like
-            string userInput = "";
+            string contName = "";
             WriteLine("Check names of all contestants in each category?");
             do
             {
-                Write("'S' for singers, 'D' for dancers, 'M' for musicians, 'O' for others, or 'continue' to skip: ");
-                userInput = ReadLine();
+                Write("'S' for singers, 'D' for dancers, 'M' for musicians, 'O' for others, or 'q' to quit: ");
+                contName = ReadLine();
                 for (int x = 0; x < talents.Length; x++)
                 {
-                    if (talents[x] == userInput)
+                    if (talents[x] == contName)
                     {
-                        Write("{0} | ", contestants[x]);
+                        Write("{0} | ", names[x]);
                     }
                 }
                 WriteLine();
             }
-            while (userInput != "continue");
-
-            //get numbers for LAST year, check if between 0-30
-            do
-            {
-                Write("Enter number of contestants last year (0-30): ");
-                lastYearContestantsCount = Convert.ToInt32(ReadLine());
-            }
-            while (lastYearContestantsCount < 0 || lastYearContestantsCount > 30);
-
-            //calculate revenue
-            revenue = FEE * thisYearContestantsCount;
-
-            WriteLine("Estimated " + revenue.ToString("C2") +
-                " in entrance fee revenue this year.");
-
-            if (thisYearContestantsCount > (lastYearContestantsCount * 2))
-                WriteLine("The competition is more than twice as big this year!");
-            else if ((thisYearContestantsCount > lastYearContestantsCount) && (thisYearContestantsCount < (lastYearContestantsCount * 2)))
-                WriteLine("The competition is bigger than ever!");
-            else
-                WriteLine("A tighter race this year! Come out and cast your vote!");
+            while (contName != "q");
 
             ReadLine();
         }
